51f9906108fa3a37c977867d3e2dbb97a57f0672
Bruno-H-Neves <bn.prog.home@gmail.com>
2021-12-29 23:16:54 +0000

    V1: System information: Date, Hour, Battery and Used Ram

--- System_info.py
+++ System_info.py
@@ -1,38 +1,38 @@
 import PySimpleGUI as sg
-import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
 import datetime
 import pytz
 import psutil
+import time
 
 sg.theme('DarkGrey7')
 
 menu_def = [[ '&File', ['&Exit',]],
             [ '&Help',['&Help','&About Us']]]
 
-column1  = [[ sg.Text('Data:', size=(15, 1)),
-              sg.Text('Hora:', size=(15, 1)),
-              sg.Text('Bateria:', size=(15, 1))],
+column1  = [[ sg.Text('Data:', size=(5, 1)),sg.Text( key='-data-', size=(10, 1)),
+              sg.Text('Hora:', size=(5, 1)),sg.Text( key='-hora-', size=(10, 1)),
+              sg.Text('Bateria:',size=(7, 1)),sg.Text( key='-bateria-', size=(5, 1)),],
             [ sg.Frame(layout=[
-            [ sg.Text('Hora:', size=(15, 1)), sg.Text('Hora:', size=(15, 1))],
-            [ sg.Text('Hora:', size=(15, 1)), sg.Text('Hora:', size=(15, 1))]
-                              ],title='Ram',title_color='red', tooltip="Center")]]
+            [ sg.Text('Total:', size=(5, 1)), sg.Text('Usada:', size=(5, 1)),sg.Text( key='-ram-', size=(10, 1))],
+            [ sg.Text('?:', size=(5, 1)), sg.Text('?:', size=(5, 1))]
+                              ],title='Ram Total Gbytes',title_color='red', tooltip="Center")]]
 
 layout   = [[ sg.Menu(menu_def, tearoff=True)],
             [ sg.Column(column1, background_color='lightblue'),sg.Button('Exit', size=(7, 1))]]
 
+window = sg.Window('Frame', layout, no_titlebar=True, location=(100,200))
 
-window = sg.Window('Frame', layout, no_titlebar=True, location=(100,200))#,size=(size_row,size_col))
 while True:
     event, values = window.read(timeout=0)
     dt_now = datetime.datetime.now(tz= pytz.UTC)
     date=r'%s-%s-%s' %(dt_now.day,dt_now.month,dt_now.year)
     hour=r'%s:%s:%s' %(dt_now.hour,dt_now.minute,dt_now.second) 
-    used_ram='Ram: %s'%(str(psutil.virtual_memory().used))
     battery = psutil.sensors_battery()
-    bat_percent ='Bat: %s '%(str(battery.percent))
-
+    window['-data-'].update(date)
+    window['-hora-'].update(hour)
+    window['-ram-'].update(psutil.virtual_memory().used/1000000)
+    window['-bateria-'].update(battery.percent)
+    time.sleep(1)
     if event in ('Exit', None):
         break
 
